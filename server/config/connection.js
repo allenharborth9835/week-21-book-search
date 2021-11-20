0bd3852e8cd7ac4debb54443e889255eac5e541c
Harborth, Allen <allenharborth9835@gmail.com>
2021-11-19 20:16:41 -0600

    get mongo db error

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-});
+}).catch(error => console.log("mongoose connect error: ", error))
 
 module.exports = mongoose.connection;
