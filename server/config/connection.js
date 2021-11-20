fcbd8892ea39322af6c931a8055cee44e1edaa0e
Harborth, Allen <allenharborth9835@gmail.com>
2021-11-19 20:57:44 -0600

    fix mango connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 }).catch(error => console.log("mongoose connect error: ", error))
 
 module.exports = mongoose.connection;
